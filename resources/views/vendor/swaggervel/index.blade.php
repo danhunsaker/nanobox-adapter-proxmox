d9b0df347eb2b64438e7b624159e896267487168
Dan Hunsaker <danhunsaker@gmail.com>
2016-12-09 05:47:39 -0700

    Update SwaggerUI
    Add/restore support for various features.
    
    Also committing the reference spec, for easy access.

--- resources/views/vendor/swaggervel/index.blade.php
+++ resources/views/vendor/swaggervel/index.blade.php
@@ -17,23 +17,26 @@ header('Access-Control-Allow-Headers: X-Requested-With');
         <link href='/vendor/swaggervel/css/screen.css' media='screen' rel='stylesheet' type='text/css'/>
         <link href='/vendor/swaggervel/css/reset.css' media='print' rel='stylesheet' type='text/css'/>
         <link href='/vendor/swaggervel/css/print.css' media='print' rel='stylesheet' type='text/css'/>
+
+        <script src='/vendor/swaggervel/lib/object-assign-pollyfill.js' type='text/javascript'></script>
         <script src='/vendor/swaggervel/lib/jquery-1.8.0.min.js' type='text/javascript'></script>
         <script src='/vendor/swaggervel/lib/jquery.slideto.min.js' type='text/javascript'></script>
         <script src='/vendor/swaggervel/lib/jquery.wiggle.min.js' type='text/javascript'></script>
         <script src='/vendor/swaggervel/lib/jquery.ba-bbq.min.js' type='text/javascript'></script>
-        <script src='/vendor/swaggervel/lib/handlebars-2.0.0.js' type='text/javascript'></script>
-        <script src='/vendor/swaggervel/lib/underscore-min.js' type='text/javascript'></script>
+        <script src='/vendor/swaggervel/lib/handlebars-4.0.5.js' type='text/javascript'></script>
+        <script src='/vendor/swaggervel/lib/lodash.min.js' type='text/javascript'></script>
         <script src='/vendor/swaggervel/lib/backbone-min.js' type='text/javascript'></script>
         <script src='/vendor/swaggervel/swagger-ui.js' type='text/javascript'></script>
-        <script src='/vendor/swaggervel/lib/highlight.7.3.pack.js' type='text/javascript'></script>
+        <script src='/vendor/swaggervel/lib/highlight.9.1.0.pack.js' type='text/javascript'></script>
+        <script src='/vendor/swaggervel/lib/highlight.9.1.0.pack_extended.js' type='text/javascript'></script>
+        <script src='/vendor/swaggervel/lib/jsoneditor.min.js' type='text/javascript'></script>
         <script src='/vendor/swaggervel/lib/marked.js' type='text/javascript'></script>
-
         <script src='/vendor/swaggervel/lib/swagger-oauth.js' type='text/javascript'></script>
 
         <!-- Some basic translations -->
-        <!-- <script src='lang/translator.js' type='text/javascript'></script> -->
-        <!-- <script src='lang/ru.js' type='text/javascript'></script> -->
-        <!-- <script src='lang/en.js' type='text/javascript'></script> -->
+        <!-- <script src='/vendor/swaggervel/lang/translator.js' type='text/javascript'></script> -->
+        <!-- <script src='/vendor/swaggervel/lang/ru.js' type='text/javascript'></script> -->
+        <!-- <script src='/vendor/swaggervel/lang/en.js' type='text/javascript'></script> -->
 
         <script type="text/javascript">
 
@@ -50,6 +53,10 @@ header('Access-Control-Allow-Headers: X-Requested-With');
                 } else {
                     url = "{!! $urlToDocs !!}";
                 }
+
+                hljs.configure({
+                  highlightSizeThreshold: 5000
+                });
 
                 // Pre load translate...
                 if (window.SwaggerTranslator) {
@@ -74,7 +81,8 @@ header('Access-Control-Allow-Headers: X-Requested-With');
                                 clientSecret: "{!! $clientSecret !!}"||"_",
                                 realm: "{!! $realm !!}"||"_",
                                 appName: "{!! $appName !!}"||"_",
-                                scopeSeparator: ","
+                                scopeSeparator: ",",
+                                additionalQueryStringParams: {}
                             });
 
                             window.oAuthRedirectUrl = "{{ url('vendor/swaggervel/o2c.html') }}";
@@ -97,7 +105,9 @@ header('Access-Control-Allow-Headers: X-Requested-With');
                     },
                     docExpansion: "list",
                     apisSorter: "alpha",
-                    showRequestHeaders: true
+                    showRequestHeaders: true,
+                    defaultModelRendering: "model",
+                    jsonEditor: true
                 });
 
                 function addApiKeyAuthorization() {
@@ -136,7 +146,7 @@ header('Access-Control-Allow-Headers: X-Requested-With');
                 <form id='api_selector'>
                     <div class='input'><input placeholder="http://example.com/api" id="input_baseUrl" name="baseUrl" type="text"/></div>
                     <div class='input'><input placeholder="api_key" id="input_apiKey" name="apiKey" type="text"/></div>
-                    <div class='input'><a id="explore" href="#" data-sw-translate>Explore</a></div>
+                    <div class='input'><a id="explore" class="header__btn" href="#" data-sw-translate>Explore</a></div>
                 </form>
             </div>
         </div>
